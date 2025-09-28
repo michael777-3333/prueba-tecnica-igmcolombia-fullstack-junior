e27251b2f141cccbad549e5cf06bfe995129cace
Michael Stiven Sarmiento Porras <131264034+michael777-3333@users.noreply.github.com>
2025-09-28 10:09:39 -0500

    feat: add inertia and model post

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users', UserController::class);
+
+// Posts (relacionados a usuarios)
+Route::resource('posts', PostController::class);
