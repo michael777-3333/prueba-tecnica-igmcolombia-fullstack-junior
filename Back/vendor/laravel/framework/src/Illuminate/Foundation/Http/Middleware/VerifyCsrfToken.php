8d612c934f0ee85747d8196bba0d2eff955d82f2
Michael Stiven Sarmiento Porras <131264034+michael777-3333@users.noreply.github.com>
2025-09-30 08:58:59 -0500

    feat: config cors

--- Back/vendor/laravel/framework/src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
+++ Back/vendor/laravel/framework/src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
@@ -39,7 +39,13 @@ class VerifyCsrfToken
      *
      * @var array<int, string>
      */
-    protected $except = [];
+    protected $except = [
+        'api/*',
+        'auth/*',
+        'login',
+        'logout',
+        'register',
+    ];
 
     /**
      * The globally ignored URIs that should be excluded from CSRF verification.
