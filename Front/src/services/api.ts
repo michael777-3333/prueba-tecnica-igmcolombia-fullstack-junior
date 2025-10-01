9be2d05cbef31a419ed6461b166833783d5a5818
Michael Stiven Sarmiento Porras <131264034+michael777-3333@users.noreply.github.com>
2025-09-30 23:28:11 -0500

    fix: config url api

--- Front/src/services/api.ts
+++ Front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Configuración base de la API
-const API_BASE_URL = 'http://localhost:81/api'
+const API_BASE_URL = 'http://3.144.137.244/api'
 
 // Crear instancia de axios
 const api = axios.create({
